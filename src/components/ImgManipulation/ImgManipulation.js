f66d1c424e98078e3562471cee84f25c9e13e6a8
Nicky <mt@se7ensky.com>
2021-01-24 20:11:14 +0200

    test1

--- src/components/ImgManipulation/ImgManipulation.js
+++ src/components/ImgManipulation/ImgManipulation.js
@@ -14,6 +14,7 @@ import { updateAnnotationFile } from '../../ajax/requests';
 
 import { PLUS, MINUS } from '../../static/constants';
 import './ImgManipulation.sass';
+//test1
 const ImgManipulation = ({ currentAnnotation, area, zoomData }) => {
     const ref = useRef(null);
     const refParent = useRef(null);
